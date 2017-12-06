b7e204c91a7461dbd2775a7c3f31ac5df5eb2d5e
ewe <ewelina.bakala@gmail.com>
2017-12-06 13:06:16 -0300

    correct resolution positivo

--- desktop/src/main/java/miceta/game/java/miCetaDesktop.java
+++ desktop/src/main/java/miceta/game/java/miCetaDesktop.java
@@ -10,8 +10,9 @@ public class miCetaDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.height = 1024; // 1366 screen size
-		config.width = 600; //768
+
+		config.width = 1366;
+		config.height = 768;
 		System.out.println(System.getProperty("java.library.path")+"  "+Core.NATIVE_LIBRARY_NAME);
 		System.loadLibrary("opencv_java2413");
 		new LwjglApplication(new miCeta(), config);
