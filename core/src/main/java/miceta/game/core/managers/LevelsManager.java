13f168cecf3d023ce17c6d2a12cdd06c12b71acb
ewe <ewelina.bakala@gmail.com>
2018-01-02 11:14:46 -0300

    fix last level with wrong operations

--- core/src/main/java/miceta/game/core/managers/LevelsManager.java
+++ core/src/main/java/miceta/game/core/managers/LevelsManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
  //
 public class LevelsManager {
+     public static final String TAG = LevelsManager.class.getName();
 
     private int level;
     private int operation_index =0;
@@ -23,7 +24,6 @@ public class LevelsManager {
 
     //Singleton
     public static LevelsManager getInstance() {
-
         if (instance == null){
             instance = new LevelsManager();
         }
@@ -31,16 +31,14 @@ public class LevelsManager {
     }
 
     private LevelsManager(){
-
         level = GamePreferences.instance.getLast_level();
-
         System.out.println("LEVEL " + level);
         operation_index = GamePreferences.instance.getOperation_index();
         load_csv();
+        get_List_of_numbers(level);
     }
 
     private void load_csv(){
-
         FileHandle handle = Gdx.files.internal("levels/ejemploNiveles.csv");
         String s_level = handle.readString();
 
@@ -65,45 +63,34 @@ public class LevelsManager {
         index =0;
 
         for (int i = 0; i < level_tope; i++) {
-
             scanner = new Scanner(lines[i+1]);
             scanner.useDelimiter("\\r");
 
             while ((scanner.hasNext())) {
-
                 String text_aux = scanner.next();
                 subLines[i][index] = text_aux;
-                System.out.println("[" + i + "-" + index + " " + subLines[i][index] + "-]");
+                System.out.println("[ i -> " + i + "- index -> " + index + " sub[i][index]-> " + subLines[i][index] + "-]");
             }
             scanner.close();
         }
 
         for (int j = 0; j < level_tope;j++){
-
             index = 0;
             Scanner scanner_2 = new Scanner(subLines[j][index]);
             scanner_2.useDelimiter(";");
-
             while ((scanner_2.hasNext())&&(index<5)) {
                 String text =  scanner_2.next();
                 subLines[j][index] = text;
-                System.out.println("[" + j + "-" + index + " " + subLines[j][index] + "-]");
+               // System.out.println("[j-> " + j + "- index -> " + index + " sub[j][index]->" + subLines[j][index] + "-]");
                 index++;
             }
-
             scanner_2.close();
         }
-        get_List_of_numbers(level);
-
     }
 
 
-
-
-
-
     public void get_List_of_numbers(int level){
-
+        list_to_play.clear();
         Integer number  = 0;
 
         if (level == level_tope) {
@@ -111,26 +98,20 @@ public class LevelsManager {
         }
         try {
             Scanner scanner = new Scanner(subLines[level][4]);
-            list_to_play.clear();
-
             while ((scanner.hasNext())) {
 
-                System.out.println("Number es :" + number);
-
                 number = (new Integer(scanner.next()).intValue());
+                Gdx.app.log(TAG,"Number to play :" + number+" level "+level);
                 list_to_play.add(number);
                 System.out.println(number.toString());
             }
 
         }
-
         catch (java.lang.NullPointerException e) {
-
             Integer tope = (new Integer(subLines[level][3]).intValue());
-            System.out.println("Tope es" + tope);
-
+            Gdx.app.log(TAG,"=== Numero tope es " + tope+" level "+level);
             int rand =0;
-            int previus_rand = 0;
+            int previous_rand = 0;
 
             for (int i=0; i < tope; i++){
 
@@ -141,26 +122,21 @@ public class LevelsManager {
                     min =1;
                 }
 
-                while ((rand == previus_rand)) {
+                while ((rand == previous_rand)) {
                     rand = MathUtils.random(min, max);
                 }
 
-                previus_rand = rand;
-                // list_to_play.clear();
+                previous_rand = rand;
                 list_to_play.add(rand);
-                System.out.println("LEVEL" + level + "RAND " + rand);
-                System.out.flush();
+                Gdx.app.log(TAG,"LEVEL" + level + " index "+ i+" RAND " + rand);
+
             }
             operation_index =0;
         }
     }
 
     public void up_level(){
-
-        System.out.println("level tope esss " + level_tope);
-
         if (level < level_tope -1) {
-            list_to_play.clear();
             operation_index =0;
             level++;
         }
@@ -168,47 +144,33 @@ public class LevelsManager {
             operation_index =0;
             level = 0; //reinicio los niveles
         }
+        get_List_of_numbers(level);
     }
 
     public int get_number_to_play(){
-
-        System.out.println("LISTA " + list_to_play.size());
         return list_to_play.get(operation_index);
     }
 
-    public void up_operation_index(){ //REVISAR ESTA FUNCION
-
+    public void up_operation_index(){
         operation_index ++;
-        System.out.println("========================================== Level: " + level);
-
-        System.out.println("========================================== Operation Index: " + operation_index);
-
         quantity_operation = (new Integer (subLines[level][3]).intValue());
 
-        System.out.println("========================================== quantity_operation " + quantity_operation);
-
-        if (operation_index == quantity_operation){ //Ojo con Esto! puede que sea mejor el ==
-
-            System.out.println("operation_index");
+        if (operation_index >= quantity_operation){ //Ojo con Esto! puede que sea mejor el ==
+            Gdx.app.log(TAG, "all operations ready! "+operation_index);
             up_level();
-            operation_index = 0;
-            get_List_of_numbers(level);
-        }
 
+        }
     }
 
     public int get_level_size(){
-
         return level_tope;
     }
 
     public int get_level(){
-
         return level;
     }
 
     public int get_operation_index(){
-
         return operation_index;
     }
 
